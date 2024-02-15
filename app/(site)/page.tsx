80138d3269c109340328275bd2477767b9011593
ianirudhkhabya <ianirudhkhabya@gmail.com>
2024-02-15 13:14:23 +0530

    Environment Setup

---
+++ app/(site)/page.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div
+      className="
+        flex
+        flex-col
+        justify-center
+        min-h-full
+        py-12
+        sm:px-6
+        lg:px-8
+        bg-gray-100
+      "
+    >
+      <div
+        className="
+          sm:mx-auto
+          sm:w-full
+          sm:max-w-md
+        "
+      >
+        <Image
+          alt="Logo"
+          height={64}
+          width={64}
+          className="mx-auto w-auto"
+          src="/img/logo.png"
+        />
+        <h2
+          className="
+            mt-6
+            text-3xl
+            text-center
+            font-bold
+            tracking-tight
+            text-gray-900
+          "
+        >
+          Sign in to your account
+        </h2>
+      </div>
+    </div>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import { Inter } from "next/font/google";
+
+import type { Metadata } from "next";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Messenger Clone",
+  description: "Messenger chat application",
+  icons: {
+    icon: [
+      { url: "/meta/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/meta/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: {
+      url: "/meta/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+  },
+  metadataBase: new URL("https://localhost:3000/"),
+  openGraph: {
+    url: "https://localhost:3000/",
+    title: "Messenger Clone",
+    description: "Messenger chat application",
+    type: "website",
+  },
+  manifest: "/meta/site.webmanifest",
+  keywords: ["Messenger", "Clone", "Chat", "Free", "ianirudhkhabya"],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>{children}</body>
+    </html>
+  );
+}
