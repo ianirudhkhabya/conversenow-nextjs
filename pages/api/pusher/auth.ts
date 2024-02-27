e6759379098c919ca279a3fb5f80f8901a320cc7
ianirudhkhabya <ianirudhkhabya@gmail.com>
2024-02-27 05:35:33 +0530

    Add dependencies and update import paths

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/libs/authOptions";
 import { pusherServer } from "@/libs/pusher";
 
 export default async function handler(
